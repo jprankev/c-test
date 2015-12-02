a300e88d7797110eb018005ab25c5c7539bf3e19
prankev8325 <prankev8325@KEMC3250013.siast.sk.ca>
2015-12-02 11:39:15 -0600

    init commit

---
+++ c-test.X/build/default/debug/_ext/1472/main.p1.d
@@ -0,0 +1,8 @@
+ build/default/debug/_ext/1472/main.d  \
+ build/default/debug/_ext/1472/main.p1:  \
+ ../main.c  \
+../function_prototypes.h  \
+../Configuration_Bits.h  \
+../my_definitions.c  \
+../port_init.c  \
+../Timers&Interupts.c 
